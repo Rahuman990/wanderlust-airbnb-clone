dab921be001b12bd22a4995324dd2f0865a25b73
Rahuman990 <rahumanramgiri990@gmail.com>
2025-05-17 10:23:30 +0530

    Add files via upload

--- user.js
+++ user.js
@@ -1,14 +1,32 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync");
+const User = require("../models/user.js");
+const passport = require("passport");
+const { saveRedirectUrl}=require("../middleware.js");
 
-const userSchema = new Schema({
-    email:{
-        type: String,
-        required:true,
-    },
-});
+const userController = require("../controllers/users.js");
 
-userSchema.plugin(passportLocalMongoose);
+//for signup
+router.get("/signup",userController.renderSignupForm);
+router.post(
+    "/signup",
+    wrapAsync(userController.signup)
+);
 
-module.exports = mongoose.model("User",userSchema);
+//for login
+router.get("/login",userController.renderLoginForm);
+router.post(
+    "/login",
+    saveRedirectUrl,
+    passport.authenticate("local",{
+        failureRedirect: "/login",
+        failureFlash:true,
+    }),
+    userController.login
+);
+
+//for log out
+router.get("/logout",userController.logout);
+
+module.exports = router;
